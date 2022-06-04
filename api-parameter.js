ef98fcf0b10eeef71f80d0cc5f0f5642f1919015
Md Arif Hussain <67943567+arifweb2020@users.noreply.github.com>
2022-06-04 12:55:49 +0530

    Update api-parameter.js

--- api-parameter.js
+++ api-parameter.js
@@ -14,3 +14,37 @@
     }
     getData()
   }, [])
+  
+  
+  // for login api
+  
+   export const LoginAsync = createAsyncThunk(
+   "login/loginApi",
+   async (args) => {
+    const { user, password } = args;
+    const urlParam = JSON.stringify({
+        username: user,
+        password: password,
+      });
+     // axios.request can be used once it is configured by default using axios.create
+     const response = await axios.post(`${endpointURL}/login`, urlParam);
+     //  console.log("score card response is : " + JSON.stringify(response.data));
+     return response;
+   }
+ );
+    
+    // for get method
+    
+      export const invoiceDetailsAsync = createAsyncThunk(
+    "invoice/invoiceDetails",
+    async (args) => {
+          const {refId} = args
+        // axios.request can be used once it is configured by default using axios.create
+       const response = await axios.get(`${endpointURL}/invoice/invoiceDetails`, {
+           params: {
+                referenceId:refId
+           }
+         })
+        return response;
+    }
+);
