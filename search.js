6eb10ccf0160a076a40a37188b8aedb1f47a30e5
Md Arif Hussain <67943567+arifweb2020@users.noreply.github.com>
2022-06-11 19:58:48 +0530

    Update search.js

--- search.js
+++ search.js
@@ -289,3 +289,118 @@ function Search(props) {
 }
 
 export default Search;
+
+
+7> search pagination
+
+import React, { useEffect, useState } from 'react';
+import Table from '../../components/Table';
+import ReactPaginate from 'react-paginate';
+
+function Search(props) {
+    const [data, setData] = useState([])
+    const [query, setQuery] = useState("")
+    const [offset, setOffset] = useState(0);
+    const [perPage] = useState(10);
+    const [pageCount, setPageCount] = useState(0)
+
+    useEffect(() => {
+        const getData = async () => {
+            const res = await fetch("https://jsonplaceholder.typicode.com/albums")
+            const res1 = await res.json()
+            setData(res1)
+            setPageCount(Math.ceil(res1.length / perPage))
+        }
+        if (query.length === 0 || query.length > 2) {
+            getData()
+
+        }
+
+    }, [query, perPage])
+
+    console.log(query)
+    console.log(data.filter((val) => val.title.toLowerCase().includes("quidem")))
+
+    const keys = ["title", "id", "userId"]
+
+    // console.log(data[0]["title"])
+    const search = (mydata) => {
+        return mydata.filter((item) =>
+            //item.title.toString().toLowerCase().includes(query) || item.id.toString().toLowerCase().includes(query) || item.userId.toString().toLowerCase().includes(query)
+            keys.some((key) => item[key].toString().toLowerCase().includes(query))
+        ).slice(offset, offset + perPage)
+    }
+
+    const handlePageClick = (e) => {
+        const selectedPage = e.selected;
+        setOffset(selectedPage + 1)
+    };
+
+    return (
+        <div style={{ margin: "0 auto", maxWidth: "70%", marginTop: "30px" }}>
+            <h1>Search</h1>
+            <input type="text" placeholder='search...' onChange={(e) => setQuery(e.target.value)} />
+
+            <Table list={search(data)} />
+
+            <ReactPaginate
+                previousLabel={"prev"}
+                nextLabel={"next"}
+                breakLabel={"..."}
+                breakClassName={"break-me"}
+                pageCount={pageCount}
+                marginPagesDisplayed={2}
+                pageRangeDisplayed={5}
+                onPageChange={handlePageClick}
+                containerClassName={"pagination justify-content-center"}
+                pageClassName={"page-item"}
+                pageLinkClassName={"page-link"}
+                previousClassName={"page-item"}
+                previousLinkClassName={"page-link"}
+                nextClassName={"page-item"}
+                nextLinkClassName={"page-link"}
+                //  breakClassName={"page-item"}
+                breakLinkClassName={"page-link"}
+                activeClassName={"active"}
+            />
+        </div>
+    );
+}
+
+export default Search;
+
+import React from 'react';
+
+function Table({ list }) {
+    return (
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>SNo</th>
+                        <th>Title</th>
+                        <th>userId</th>
+                    </tr>
+
+                </thead>
+                <tbody>
+                    {list.map((ele, i) => {
+                        return <tr key={i}>
+                            <td >{ele.id}</td>
+                            <td >{ele.title}</td>
+                            <td >{ele.userId}</td>
+                        </tr>
+                    })}
+
+                </tbody>
+            </table>
+
+
+            <td>
+
+            </td>
+        </div>
+    );
+}
+
+export default Table;
