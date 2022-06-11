06f3b9f96e8b5e59cc9465d3522d788b80472c5f
Md Arif Hussain <67943567+arifweb2020@users.noreply.github.com>
2022-06-11 19:53:47 +0530

    Update search.js

--- search.js
+++ search.js
@@ -120,3 +120,172 @@ function Table({ list }) {
 }
 
 export default Table;
+
+
+3> 0 to 10 search
+
+import React, { useEffect, useState } from 'react';
+
+
+function Search(props) {
+    const [data, setData] = useState([])
+    const [query, setQuery] = useState("")
+
+    useEffect(() => {
+        const getData = async () => {
+            const res = await fetch("https://jsonplaceholder.typicode.com/albums")
+            const res1 = await res.json()
+            setData(res1)
+            // setPageCount(Math.ceil(setData.length / perPage))
+        }
+        if (query.length === 0 || query.length > 2) {
+            getData()
+
+        }
+
+    }, [query])
+
+    return (
+        <div style={{ margin: "0 auto", maxWidth: "70%", marginTop: "30px" }}>
+            <h1>Search</h1>
+            <input type="text" placeholder='search..' onChange={(e)=>setQuery(e.target.value)}/>
+
+            {
+                data.filter((ss)=>ss.title.toLowerCase().includes(query)).slice(0, 10).map((ele,i)=>{
+                    return <p key={i}>{ele.title}</p>
+                })
+            }
+        </div>
+    );
+}
+
+export default Search;
+
+
+
+4> dropdown search
+
+import { useState } from "react";
+import "./styles.css";
+var data = require("./MOCK_DATA.json");
+
+export default function App() {
+  const [value, setValue] = useState("");
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const onSearch = (searchTerm) => {
+    setValue(searchTerm);
+    // our api to fetch the search result
+    console.log("search ", searchTerm);
+  };
+
+  return (
+    <div className="App">
+      <h1>Search</h1>
+
+      <div className="search-container">
+        <div className="search-inner">
+          <input type="text" value={value} onChange={onChange} />
+          <button onClick={() => onSearch(value)}> Search </button>
+        </div>
+        <div className="dropdown">
+          {data
+            .filter((item) => {
+              const searchTerm = value.toLowerCase();
+              const fullName = item.full_name.toLowerCase();
+
+              return (
+                searchTerm &&
+                fullName.startsWith(searchTerm) &&
+                fullName !== searchTerm
+              );
+            })
+            .slice(0, 10)
+            .map((item) => (
+              <div
+                onClick={() => onSearch(item.full_name)}
+                className="dropdown-row"
+                key={item.full_name}
+              >
+                {item.full_name}
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+
+5 > final one search with pagination
+
+
+import React, { useEffect, useState } from 'react';
+import ReactPaginate  from 'react-paginate';
+
+
+function Search(props) {
+    const [data, setData] = useState([])
+    const [query, setQuery] = useState("")
+    const [pageCount, setPageCount] = useState(0)
+    const [perPage] = useState(10);
+    const [offset, setOffset] = useState(0);
+    useEffect(() => {
+        const getData = async () => {
+            const res = await fetch("https://jsonplaceholder.typicode.com/albums")
+            const res1 = await res.json()
+            setData(res1)
+            setPageCount(Math.ceil(res1.length / perPage))
+        }
+        if (query.length === 0 || query.length > 2) {
+            getData()
+
+        }
+
+    }, [perPage, query])
+
+    const handlePageClick = (e) => {
+        const selectedPage = e.selected;
+        setOffset(selectedPage + 1)
+    };
+
+    return (
+        <div style={{ margin: "0 auto", maxWidth: "70%", marginTop: "30px" }}>
+            <h1>Search</h1>
+            <input type="text" placeholder='search..' onChange={(e) => setQuery(e.target.value)} />
+
+            {
+                data.filter((ss) => ss.title.toLowerCase().includes(query)).slice(offset, offset + perPage).map((ele, i) => {
+                    return <p key={i}>{ele.title}</p>
+                })
+            }
+
+            <ReactPaginate
+                previousLabel={"prev"}
+                nextLabel={"next"}
+                breakLabel={"..."}
+                breakClassName={"break-me"}
+                pageCount={pageCount}
+                marginPagesDisplayed={2}
+                pageRangeDisplayed={5}
+                onPageChange={handlePageClick}
+                containerClassName={"pagination justify-content-center"}
+                pageClassName={"page-item"}
+                pageLinkClassName={"page-link"}
+                previousClassName={"page-item"}
+                previousLinkClassName={"page-link"}
+                nextClassName={"page-item"}
+                nextLinkClassName={"page-link"}
+                // breakClassName={"page-item"}
+                breakLinkClassName={"page-link"}
+                activeClassName={"active"}
+            />
+        </div>
+    );
+}
+
+export default Search;
