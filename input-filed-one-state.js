00b21a3387fe10e8397f44214f0776993366c015
Md Arif Hussain <67943567+arifweb2020@users.noreply.github.com>
2022-06-21 12:27:05 +0530

    Create input-filed-one-state.js

---
+++ input-filed-one-state.js
@@ -0,0 +1,232 @@
+
+// add user.js
+import React, { useState } from "react";
+import axios from 'axios'
+import { useHistory } from "react-router-dom";
+
+const AddUser = () => {
+  let history = useHistory();
+  const [user, setUser] = useState({
+    name: "",
+    username: "",
+    email: "",  
+    phone: "",
+    website: ""
+  });
+
+  const { name, username, email, phone, website } = user;
+  const onInputChange = e => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    await axios.post("http://localhost:3003/users", user);
+    history.push("/");
+  };
+  return (
+    <div className="container">
+      <div className="w-75 mx-auto shadow p-5">
+        <h2 className="text-center mb-4">Add A User</h2>
+        <form onSubmit={e => onSubmit(e)}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter Your Name"
+              name="name"
+              value={name}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter Your Username"
+              name="username"
+              value={username}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control form-control-lg"
+              placeholder="Enter Your E-mail Address"
+              name="email"
+              value={email}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter Your Phone Number"
+              name="phone"
+              value={phone}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter Your Website Name"
+              name="website"
+              value={website}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <button className="btn btn-primary btn-block">Add User</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AddUser;
+
+
+// user.js
+
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+
+const User = () => {
+  const [user, setUser] = useState({
+    name: "",
+    username: "",
+    email: "",
+    phone: "",
+    webiste: ""
+  });
+  const { id } = useParams();
+  useEffect(() => {
+    loadUser();
+  }, []);
+  const loadUser = async () => {
+    const res = await axios.get(`http://localhost:3003/users/${id}`);
+    setUser(res.data);
+  };
+  return (
+    <div className="container py-4">
+      <Link className="btn btn-primary" to="/">
+        back to Home
+      </Link>
+      <h1 className="display-4">User Id: {id}</h1>
+      <hr />
+      <ul className="list-group w-50">
+        <li className="list-group-item">name: {user.name}</li>
+        <li className="list-group-item">user name: {user.username}</li>
+        <li className="list-group-item">email: {user.email}</li>
+        <li className="list-group-item">phone: {user.phone}</li>
+        <li className="list-group-item">website: {user.website}</li>
+      </ul>
+    </div>
+  );
+};
+
+export default User;
+
+// edit user
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useHistory, useParams } from "react-router-dom";
+
+const EditUser = () => {
+  let history = useHistory();
+  const { id } = useParams();
+  const [user, setUser] = useState({
+    name: "",
+    username: "",
+    email: "",
+    phone: "",
+    website: ""
+  });
+
+  const { name, username, email, phone, website } = user;
+  const onInputChange = e => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  useEffect(() => {
+    loadUser();
+  }, []);
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    await axios.put(`http://localhost:3003/users/${id}`, user);
+    history.push("/");
+  };
+
+  const loadUser = async () => {
+    const result = await axios.get(`http://localhost:3003/users/${id}`);
+    setUser(result.data);
+  };
+  return (
+    <div className="container">
+      <div className="w-75 mx-auto shadow p-5">
+        <h2 className="text-center mb-4">Edit A User</h2>
+        <form onSubmit={e => onSubmit(e)}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter Your Name"
+              name="name"
+              value={name}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter Your Username"
+              name="username"
+              value={username}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control form-control-lg"
+              placeholder="Enter Your E-mail Address"
+              name="email"
+              value={email}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter Your Phone Number"
+              name="phone"
+              value={phone}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter Your Website Name"
+              name="website"
+              value={website}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <button className="btn btn-warning btn-block">Update User</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default EditUser;
