81883bebd28d42f108460a013544cb9598ee0a7f
Md Arif Hussain <67943567+arifweb2020@users.noreply.github.com>
2022-08-18 18:59:26 +0530

    Create jquery-language.js

---
+++ jquery-language.js
@@ -0,0 +1,18 @@
+		    <div class="col-md-2 col-sm-4 col-xs-12 mbl-link hidden-xs">
+		
+			    <div class="social-icons text-left">
+				    <a class="facebook" href="https://www.facebook.com/HomesInAbroadcom-1975060876041342/" target="_blank"><i class="fa fa-facebook"></i></a>
+				    <a class="twitter" href="https://twitter.com/homesinabroad" target="_blank"><i class="fa fa-twitter"></i></a>
+				    <a class="pinterest" href="https://www.instagram.com/homesinabroad/" target="_blank"><i class="fa fa-instagram"></i></a>
+				    <a class="linkedin" href="https://www.linkedin.com/in/homes-in-abroad-a2b765175/" target="_blank"><i class="fa fa-linkedin"></i></a>
+			    </div>
+		    </div>
+		    <div class="col-md-2 col-sm-2 col-xs-12 mbl-dr" id="google_translate_element"></div>
+	    </div>
+		
+	
+	<div id="google_translate_element"></div><script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: 'ar,de,el,en,fr,ml,zh-CN', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+}
+</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
