48fcded426607d06134a1b1e62c279ddf072f608
Md Arif Hussain <67943567+arifweb2020@users.noreply.github.com>
2023-01-09 13:06:00 +0530

    Create prop-types-react.js

---
+++ prop-types-react.js
@@ -0,0 +1,16 @@
+npm install --save prop-types
+https://designcode.io/react-hooks-handbook-proptypes
+
+import PropTypes from 'prop-types'
+
+function HelloWorldComponent({ name }) {
+  return (
+    <div>Hello, {name}</div>
+  )
+}
+
+HelloWorldComponent.propTypes = {
+  name: PropTypes.string
+}
+
+export default HelloWorldComponent
