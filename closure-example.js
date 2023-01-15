e1ac1191e58fb4f212206f811b8351f0c3cae7be
Md Arif Hussain <67943567+arifweb2020@users.noreply.github.com>
2023-01-15 13:55:42 +0530

    Update closure-example.js

--- closure-example.js
+++ closure-example.js
@@ -1,3 +1,7 @@
+Closure :Function bundled with its lexical environment is known as a closure.
+Whenever function is returned, even if its vanished in execution context but still it
+remembers the reference it was pointing to. Its not just that function alone it returns but the entire closure and that's where it becomes interesting 
+
 Async code
 Closures are commonly used with async code, for example: sending a POST request using the Fetch API:
 function getData(url) {
