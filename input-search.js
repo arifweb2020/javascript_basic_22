7d08d04c97a854559e5f761d6eb1daadbf6279ad
Md Arif Hussain <67943567+arifweb2020@users.noreply.github.com>
2023-01-20 18:37:53 +0530

    Update input-search.js

--- input-search.js
+++ input-search.js
@@ -1,3 +1,67 @@
+// onclck buutton wid data
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+
+
+  const [data, setData] = useState([]);
+  const [fdata, setFdata] = useState([])
+  console.log(fdata)
+  const [search, setSearch] = useState("")
+
+
+
+  useEffect(() => {
+
+    (async () => {
+      const res = await fetch('https://jsonplaceholder.typicode.com/users')
+      const res1 = await res.json();
+      setData(res1)
+      setFdata(res1)
+    })()
+  }, [])
+
+  const searchHandler = (text) => {
+   
+    setSearch(text)
+    console.log(text)
+    if (text !== "") {
+      const filteredData = data.filter((item) => {
+        return Object.values(item).join('').toLowerCase().includes(search.toLowerCase())
+      })
+      setFdata(filteredData)
+    }
+    else {
+      setFdata(data)
+    }
+  }
+
+
+
+  return (
+    <div className="App">
+      
+        <input type="text" value={search} onChange={(e) => setSearch(e.target.value)}
+
+        />
+      
+      <button onClick={() => searchHandler(search)}>search</button>
+
+      <br />
+
+      {fdata.map((ele) => {
+        return <>{<h1>{ele.name} - {ele.username}</h1>}</>
+      })
+
+      }
+
+
+    </div>
+  );
+}
+export default App;
+
 // input button search
 
 import React, { useState, useEffect } from 'react';
