3da89d2c19f86d31a9679b0f478963742ef0c876
Md Arif Hussain <67943567+arifweb2020@users.noreply.github.com>
2023-01-20 18:05:11 +0530

    Update input-search.js

--- input-search.js
+++ input-search.js
@@ -7,6 +7,63 @@ function App() {
 
 
   const [data, setData] = useState([]);
+  const [fdata, setFdata] = useState([])
+  const [search, setSearch] = useState("")
+
+
+
+  useEffect(() => {
+
+    (async () => {
+      const res = await fetch('https://jsonplaceholder.typicode.com/users')
+      const res1 = await res.json();
+      setData(res1)
+    })()
+  }, [])
+
+  const searchHandler = (text) => {
+    setSearch(text)
+    console.log(text)
+    if (text !== "") {
+      const filteredData = data.filter((item) => {
+        return Object.values(item).join('').toLowerCase().includes(search.toLowerCase())
+      })
+      setFdata(filteredData)
+    }
+    else{
+      setFdata(data)
+    }
+  }
+
+  return (
+    <div className="App">
+
+      <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} />
+      <button onClick={() => searchHandler(search)}>search</button>
+
+      <br />
+
+      {fdata.map((ele) => {
+        return <>{search && (<h1>{ele.name} - {ele.username}</h1>)}</>
+      })
+
+      }
+
+
+    </div>
+  );
+}
+export default App;
+
+// input button test
+
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+
+
+  const [data, setData] = useState([]);
 
   const [search, setSearch] = useState("")
 
