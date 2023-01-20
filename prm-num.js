0231b06632df4a251a8f281a5d72f488c4265ea2
Md Arif Hussain <67943567+arifweb2020@users.noreply.github.com>
2023-01-20 20:17:53 +0530

    Update prm-num.js

--- prm-num.js
+++ prm-num.js
@@ -3,3 +3,19 @@ const x = "arif";
 for(let i = x.length -1 ; i >=0 ; i--){
     console.log(x[i])
 }
+
+
+function isPrime(n) {
+
+    if (n < 2){
+         return `${n} is not a prime` 
+    }
+     
+    for (let i = 2; i < n; i++) {
+        if (n % i === 0) {
+            return `${n} is not a prime number`
+        }
+    }
+    return `${n} is a prime number`
+}
+console.log(isPrime(3));
