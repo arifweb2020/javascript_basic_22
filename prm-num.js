dd3a23b921a41e46f730e8e56ededbb8959f6cc5
Md Arif Hussain <67943567+arifweb2020@users.noreply.github.com>
2023-01-20 22:16:07 +0530

    Update prm-num.js

--- prm-num.js
+++ prm-num.js
@@ -19,3 +19,36 @@ function isPrime(n) {
     return `${n} is a prime number`
 }
 console.log(isPrime(3));
+
+
+// to check odd and even
+
+function odd(n){
+    
+   if(n % 2 === 0){
+       console.log("even")
+   }
+   else{
+       console.log("odd")
+   }
+  
+    
+}
+
+console.log(odd(7))
+
+
+// to print odd number
+
+function evens(ag){
+console.log("ag " , ag)
+
+for (var mg=1; mg <=ag; mg++)
+{
+  if(mg%2==0){
+    document.write(mg + "<br>")
+  }
+}
+}
+evens(12)
+
