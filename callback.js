d2928a62e4ba57b41b2948bebb5da715b847a34c
Md Arif Hussain <67943567+arifweb2020@users.noreply.github.com>
2023-01-24 19:48:37 +0530

    Update callback.js

--- callback.js
+++ callback.js
@@ -70,3 +70,76 @@ register(()=>{
 
 console.log("other app work here")
 
+
+// 3 how to over come from callback hell using promise;
+
+
+function register(){
+    
+    return new Promise ((resolve,reject)=>{
+        console.log("register user")
+        resolve()
+    })
+    
+    
+}
+
+function sendemail(){
+    
+     return new Promise ((resolve,reject)=>{
+         console.log("send email")
+        resolve()
+    })
+   
+    
+}
+
+
+function login(){
+     return new Promise ((resolve,reject)=>{
+        console.log("user login")
+        resolve()
+    })
+    
+    
+}
+
+function getuser(){
+    
+     return new Promise ((resolve,reject)=>{
+          console.log("get user data")
+        resolve()
+    })
+  
+    
+}
+
+function displayuser(){
+    
+     return new Promise ((resolve,reject)=>{
+         console.log("diplay user data")
+        resolve()
+    })
+   
+    
+}
+
+register().then(sendemail).then(login).then(getuser).then(displayuser).catch((err)=> "some went wrong")
+
+
+console.log("other app work here")
+
+// using async await this make code neat clean
+
+async function auth(){
+    await register();
+      await sendemail();
+        await login();
+          await getuser();
+            await displayuser();
+}
+
+auth().then(()=>{
+    console.log("now call me ")
+})
+
