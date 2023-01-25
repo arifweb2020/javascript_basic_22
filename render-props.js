75eca031d8d17f333dc1c5bce9d847a0454b1afa
Md Arif Hussain <67943567+arifweb2020@users.noreply.github.com>
2023-01-25 12:20:07 +0530

    Create render-props.js

---
+++ render-props.js
@@ -0,0 +1,47 @@
+App.js
+
+import Counter
+
+ <Counter render={(count, increment, decrement) => (<RenBtn cnt={count} inc={increment} dec={decrement} />)} />
+
+
+// Counter.js
+
+import React, { useState } from 'react';
+
+function Counter(props) {
+    const [count, setCount] = useState(0)
+
+    const increment = () => {
+        setCount((prev) => prev + 1)
+    }
+
+    const decrement = () => {
+        setCount((prev) => prev - 1)
+    }
+
+    return (
+        <div>
+            <h1>Render props counter</h1>
+            {props.render(count, increment, decrement)}
+        </div>
+    );
+}
+
+export default Counter;
+
+// RenBtn.js
+
+import React from 'react';
+
+function RenBtn({cnt,inc,dec}) {
+    return (
+        <div>
+            {cnt}
+            <button onClick={inc}>+</button>
+            <button onClick={dec}>-</button>
+        </div>
+    );
+}
+
+export default RenBtn;
