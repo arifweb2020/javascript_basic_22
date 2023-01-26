d57e86e394c8183665c063e2c92a82c96bc8ac2c
Md Arif Hussain <67943567+arifweb2020@users.noreply.github.com>
2023-01-26 21:31:35 +0530

    Update load-more.js

--- load-more.js
+++ load-more.js
@@ -37,3 +37,94 @@ const lessData=()=>{
                }
                 
             </div>
+
+
+
+
+// 2nd way
+
+import "./styles.css";
+import { useState } from "react";
+export default function App() {
+  const [visibleBlogs, setVisibleBlogs] = useState(2);
+  const [seeMe, setSeeMe] = useState(false);
+  const [blogs, setBlog] = useState([
+    {
+      name: "pavan",
+      age: 22,
+      id: 1
+    },
+    {
+      name: "pavu",
+      age: 22,
+      id: 2
+    },
+    {
+      name: "johan",
+      age: 22,
+      id: 3
+    },
+    {
+      name: "Deepu",
+      age: 22
+    },
+    {
+      name: "kareya",
+      age: 22,
+      id: 4
+    },
+    {
+      name: "killer",
+      age: 22,
+      id: 5
+    },
+    {
+      name: "miller",
+      age: 22,
+      id: 6
+    },
+    {
+      name: "vivek",
+      age: 22,
+      id: 7
+    },
+    {
+      name: "shashank",
+      age: 22,
+      id: 8
+    },
+    {
+      name: "facebook",
+      age: 22,
+      id: 9
+    }
+  ]);
+
+  const laodMe = () => {
+    setSeeMe(!seeMe);
+  };
+  const cardComponent = blogs.slice(0, visibleBlogs).map((blog, i) => {
+    return <p key={i}>{blog.name}</p>;
+  });
+
+  return (
+    <div>
+      {cardComponent}
+      <button
+        disabled={blogs.length === cardComponent.length}
+        type="button"
+        onClick={() => setVisibleBlogs((prev) => prev + 2)}
+      >
+        See more
+      </button>
+
+      <br />
+      <br />
+      <br />
+      <button type="button" onClick={laodMe}>
+        view name All
+      </button>
+      {seeMe && cardComponent}
+    </div>
+  );
+}
