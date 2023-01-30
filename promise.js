84c051275379f63a2a0f6b2c7dc0bde106885831
Md Arif Hussain <67943567+arifweb2020@users.noreply.github.com>
2023-01-30 16:48:25 +0530

    Update promise.js

--- promise.js
+++ promise.js
@@ -39,3 +39,53 @@ const otp = ()=>{
 reg().then(email().then((res)=>{
      console.log(res)
 })).then(otp())
+
+
+// ERROR
+
+
+
+function reg(){
+    
+    return new Promise((res,rej)=>{
+          
+          setTimeout(()=>{
+              
+              console.log("reg done")
+              res ();
+              
+          },1000)
+          
+    })
+}
+
+function email(){
+    
+    return new Promise((res,rej)=>{
+          
+          setTimeout(()=>{
+               return rej("some thing wrong");
+              console.log("email done")
+              
+              
+             
+          },2000)
+          
+    })
+}
+
+function otp(){
+    
+    return new Promise((res,rej)=>{
+          
+          setTimeout(()=>{
+              console.log("otp done")
+              res();
+          },3000)
+          
+    })
+}
+
+reg().then(email).then(otp).catch((err)=>{
+    console.log(err)
+})
