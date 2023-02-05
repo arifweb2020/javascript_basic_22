10e8543b1474e2315c951e5cb4442310e1ba7eb0
Md Arif Hussain <67943567+arifweb2020@users.noreply.github.com>
2023-02-05 20:38:05 +0530

    Update debouncing.js

--- debouncing.js
+++ debouncing.js
@@ -146,3 +146,54 @@ export default App;
             }
             </> : null
         }
+        
+   // filter search with delay
+        
+        import cities from 'cities-list'
+import { useState } from 'react'
+
+const citiesArray = Object.keys(cities)
+
+const App = () => {
+  const [filteredCities, setFilteredCities] = useState([])
+  
+  let filterTimeout
+
+const doCityFilter = query => {
+  clearTimeout(filterTimeout)
+  if (!query) return setFilteredCities([])
+
+  filterTimeout = setTimeout(() => {
+    console.log('====>', query)
+    setFilteredCities(citiesArray.filter(
+      city => city.toLowerCase().includes(query.toLowerCase())
+    ))
+  }, 500)
+}
+
+  return (
+    <div className="container">
+      <h1>Find your favourite cities</h1>
+
+      <form className="mt-3 mb-5">
+        <input
+          type="text"
+          className="px-2"
+          placeholder="search here..."
+          onChange={event => (doCityFilter(event.target.value))}
+        />
+      </form>
+
+      <div>
+        {filteredCities?.map((city, index) => (
+          <p key={index}>{city}</p>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default App
+
+
+
