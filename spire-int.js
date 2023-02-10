acd68f378bccafbf17de40960cad2bf188f7d92b
Md Arif Hussain <67943567+arifweb2020@users.noreply.github.com>
2023-02-10 12:44:09 +0530

    Create spire-int.js

---
+++ spire-int.js
@@ -0,0 +1,44 @@
+/** 
+ * thius is nav compone
+ * Author Ariuucff
+ */
+import "./styles.css";
+import {useState,useEffect} from 'react'
+
+export default function App() {
+  const [name,setName] = useState("idle")
+  const [show,setShow] = useState(false)
+
+  useEffect(()=>{
+    if(show){
+      let y = Math.ceil(Math.random() * 10)
+      setTimeout(()=>{
+        if (y > 2){
+          setName("success")
+        }
+        else{
+          setName("error")
+        }
+     
+      },1000)
+    }
+
+  },[show])
+
+const startHandler = ()=>{
+setShow(true)
+setName("start")
+}
+
+const stopHandler = ()=>{
+  setName("idle")
+}
+
+  return (
+    <div className="App">
+      {name}<br/>
+      <button onClick={startHandler}>start</button>
+      <button onClick={stopHandler}>stop</button>
+    </div>
+  );
+}
